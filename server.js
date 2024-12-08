2a6ed88145f5fc384442e8b2d1aaafe3a891891d
Raj Aryan <unknownx7258@gmail.com>
2024-12-08 22:11:36 +0530

    added yr

--- server.js
+++ server.js
@@ -17,11 +17,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Middleware
-app.use(cors({
-    origin: ['http://localhost:3000', 'https://cloudinary-rose.vercel.app'],
-    methods: ['GET', 'POST'],
-    credentials: true
-  }));
+app.use(cors());
 app.use(fileUpload());
 app.use(express.json());
 
