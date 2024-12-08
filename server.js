c2d2d10abefb03209f104e6b6736b2aed3eeff9f
Raj Aryan <unknownx7258@gmail.com>
2024-12-08 22:02:07 +0530

    solved iisus of cors and upload erro

--- server.js
+++ server.js
@@ -17,7 +17,11 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+    origin: ['http://localhost:3000', 'https://cloudinary-rose.vercel.app'],
+    methods: ['GET', 'POST'],
+    credentials: true
+  }));
 app.use(fileUpload());
 app.use(express.json());
 
